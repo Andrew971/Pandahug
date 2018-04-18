270d9cecd330bfe853d2c8e321081ff80e886cf3
Andrew971 <sordier.andrew@gmail.com>
2018-04-17 23:29:13 -0700

    ok

--- frontend/src/js/api.js
+++ frontend/src/js/api.js
@@ -1,10 +1,10 @@
 
 //dev
-export const cart = 'http://localhost:8080/cart'
-export const contact ='http://localhost:8080/contact'
-export const search ='http://localhost:8080/search'
-
-//prod
 // export const cart = 'http://localhost:8080/cart'
 // export const contact ='http://localhost:8080/contact'
 // export const search ='http://localhost:8080/search'
+
+//prod
+export const cart = 'http://Test-env-2.vtahxtnppg.ca-central-1.elasticbeanstalk.com/cart'
+export const contact ='http://Test-env-2.vtahxtnppg.ca-central-1.elasticbeanstalk.com/contact'
+export const search ='http://Test-env-2.vtahxtnppg.ca-central-1.elasticbeanstalk.com/search'
